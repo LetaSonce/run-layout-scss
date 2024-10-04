408aa13ac1973b7a74df435018c03f405215dca2
Leta Sonce <letasonce@gmail.com>
2024-10-04 13:18:38 +0300

    init: initialize project

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import '../scss/style.scss'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Start your project here!</h1>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
